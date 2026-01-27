220b409c788a16fcb4133d0b2a183aa27193c223
Akash Santra <santraakash999@gmail.com>
2026-01-27 19:56:13 +0530

    modify usermodel

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -6,15 +6,19 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullName: {
       type: String,
       required: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
       minlength: 6,
+      select: false,
     },
     profilePic: {
       type: String,
