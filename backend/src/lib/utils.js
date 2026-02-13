e8a4c44067b5e1e9bd97f0a3bf04420d8c596773
Akash Santra <santraakash999@gmail.com>
2026-02-13 19:01:04 +0530

    backend utils updated

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,17 +5,15 @@ export const generateToken = (userId, res) => {
     throw new Error("JWT_SECRET is not defined");
   }
 
-  const token = jwt.sign(
-    { userId },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    secure: true,
   });
 
   return token;
