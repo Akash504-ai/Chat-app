3d9b18fcccb1febb7f597c3cdefb9e0957651e5f
Akash Santra <santraakash999@gmail.com>
2026-02-13 18:40:19 +0530

    origin change

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ import reportRoutes from "./routes/report.routes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" }));
@@ -25,7 +25,9 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    // origin: process.env.CLIENT_URL,
+    origin:true,
     credentials: true,
   })
 );
