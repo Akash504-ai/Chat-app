f8c33f36d13de5cccdc5d3399db7658a15447dca
Akash Santra <santraakash999@gmail.com>
2026-02-27 19:32:33 +0530

    Authrout done for forg pass

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,31 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+  setupSecurityQuestions,
+  verifySecurityAnswers,
+  resetPassword,
+} from "../controllers/auth.controller.js";
+
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// Auth
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/check", protectRoute, checkAuth);
 
+// Profile
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, checkAuth);
+// 🔐 Security Questions
+router.put("/security-questions", protectRoute, setupSecurityQuestions);
+router.post("/verify-security", verifySecurityAnswers);
+router.post("/reset-password", resetPassword);
 
 export default router;
